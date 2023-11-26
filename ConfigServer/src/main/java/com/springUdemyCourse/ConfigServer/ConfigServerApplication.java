bd003f9905d61f107b2d6c9ba39946117c2de725
Unknown-Soul <ashutoshgupta1231@gmail.com>
2023-11-26 20:52:35 +0530

    docker files added

--- ConfigServer/src/main/java/com/springUdemyCourse/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/springUdemyCourse/ConfigServer/ConfigServerApplication.java
@@ -1,7 +1,6 @@
-package com.springUdemyCourse.configServer;
+package com.springUdemyCourse.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
